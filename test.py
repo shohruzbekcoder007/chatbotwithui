d66ae152b69ffc0d953835177d70672162aa9dc8
Jaloliddin Mamatmusayev <j.mamatmusayev@gmail.com>
2025-04-29 17:53:55 +0500

    add json files and change test.py for upload chromadb all jsons function

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ from retriever.langchain_chroma import count_documents, add_documents_from_json,
 # json_file_path = "./tayyor_json/gender.json"
 # json_file_path = "./tayyor_json/statistika_ishlarini_tashkil_etish_va_yuritish.json"
 # json_file_path = "./tayyor_json/davlat_va_bojxona_tashqi_savdo.json"
-json_file_path = "./tayyor_json/qumitasi_faoliyatini_tashkil_etish_pq75.json"
+# json_file_path = "./tayyor_json/qumitasi_faoliyatini_tashkil_etish_pq75.json"
 
 
 # rasmiy_statistika_togrisida_qonun_en_json = "./tayyor_json/rasmiy_statistika_togrisida_qonun_en.json"
@@ -23,14 +23,27 @@ json_file_path = "./tayyor_json/qumitasi_faoliyatini_tashkil_etish_pq75.json"
 # create_collection()
 
 # Barcha hujjatlarning sonini ko'rish
-print(f"Jami hujjatlar soni: {count_documents()}")
+print(f"Eski hujjatlar soni: {count_documents()}")
 
 # Avval barcha hujjatlarni o'chirish
 # remove_all_documents()
 # create_collection()
 
 # JSON fayldan ma'lumotlarni o'qib ChromaDB ga qo'shish
-add_documents_from_json(json_file_path)
+# add_documents_from_json(json_file_path)
 
+
+import os
+def process_all_json_files(folder_path: str = "./tayyor_json/"):
+    remove_all_documents()
+    create_collection()
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith(".json"):
+                full_path = os.path.join(root, file)
+                add_documents_from_json(full_path)
+                print(f"######################################  Qo'shilgan fayl: {full_path}")
+
+process_all_json_files("./tayyor_json/")
 # Barcha hujjatlarning sonini ko'rish
 print(f"Jami hujjatlar soni: {count_documents()}")
